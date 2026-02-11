1d2d496f984e702aca6fa0332ddcd01d23ff4183
ShanuDevCodes <shanu.dev.codes@gmail.com>
2026-02-11 06:12:42 +0530

    - ios implementation - bug fixes

--- composeApp/src/iosMain/kotlin/com/kito/core/platform/AppConfig.ios.kt
+++ composeApp/src/iosMain/kotlin/com/kito/core/platform/AppConfig.ios.kt
@@ -1,8 +1,17 @@
 package com.kito.core.platform
 
+import platform.Foundation.NSBundle
+
 actual object AppConfig {
-    actual val portalBase: String = "" // TODO: Read from iOS plist or config
-    actual val wdPath: String = ""
-    actual val supabaseUrl: String = ""
-    actual val supabaseAnonKey: String = ""
+    actual val portalBase: String
+        get() = NSBundle.mainBundle.infoDictionary?.get("PORTAL_BASE") as? String ?: ""
+
+    actual val wdPath: String
+        get() = NSBundle.mainBundle.infoDictionary?.get("WD_PATH") as? String ?: ""
+
+    actual val supabaseUrl: String
+        get() = NSBundle.mainBundle.infoDictionary?.get("SUPABASE_URL") as? String ?: ""
+
+    actual val supabaseAnonKey: String
+        get() = NSBundle.mainBundle.infoDictionary?.get("SUPABASE_ANON_KEY") as? String ?: ""
 }
